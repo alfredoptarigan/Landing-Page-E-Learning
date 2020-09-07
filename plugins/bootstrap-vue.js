30e2820e486073a103c0ae48f4d3bc2f7c492c14
Alfredo Patricius Tarigan <62396803+alfredoptarigan@users.noreply.github.com>
2020-09-07 12:09:10 +0700

    first commit

---
+++ plugins/bootstrap-vue.js
@@ -0,0 +1,5 @@
+import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
+Vue.use(BootstrapVue);
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css'
